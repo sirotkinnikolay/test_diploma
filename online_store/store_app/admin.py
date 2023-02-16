8a2510e9156f4e9132fea449fb82a7065a34ff06
sirotkin <sirotkin.nikola@mail.ru>
2023-02-16 16:52:38 +0300

    save

--- online_store/store_app/admin.py
+++ online_store/store_app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'patronymic', 'surname', 'phone', 'email', 'profile_picture']
+
+
+class CategoryProductAdmin(admin.ModelAdmin):
+    list_display = ['title_category']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title_product', 'description', 'count', 'price', 'category', 'limited_edition', 'product_picture']
+
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ['username', 'product']
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ['text', 'product', 'user_name', 'create_at']
+
+
+class UserHistoryAdmin(admin.ModelAdmin):
+    list_display = ['user_history', 'product_history', 'payment_date',
+                    'payment_method', 'payment_delivery', 'mistake_text']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(CategoryProduct, CategoryProductAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Basket, BasketAdmin)
+admin.site.register(Reviews, ReviewsAdmin)
+admin.site.register(UserHistory, UserHistoryAdmin)
--- online_store/store_app/urls.py
+++ online_store/store_app/urls.py
@@ -5,15 +5,28 @@ urlpatterns = [
     path('', StartView.as_view(), name='start'),
 
     path('logout/', AuthorLogoutView.as_view(), name='logout'),  # страница входа пользователя
-    path('register/', Register.as_view(), name='register'),  # страница регистрации
+    path('register/', RegisterView.as_view(), name='register'),  # страница регистрации
     path('login/', AuthorLoginView.as_view(), name='login'),  # страница выхода из учетной записи
 
-    path('office/', UserOffice.as_view(), name='office'),  # личный кабинет пользователя
-    path('profile_edit/', ProfileEdit.as_view(), name='profile_edit'),  # редактирование профиля пользователя
-    path('history_buy/', UserHistoryBuy.as_view(), name='history_duy'),  # история заказов пользователя
+    path('about/', AboutShopView.as_view(), name='about'),  # страница с информацией о магазине
+    path('account/', UserOfficeView.as_view(), name='account'),  # личный кабинет пользователя
+    path('cart/', UserCartView.as_view(), name='cart'),  # корзина пользователя
+    path('catalog/', AllProductCatalogView.as_view(), name='catalog'),  # каталог товаров с фильтром и сортировкой
+    path('history_order/', UserHistoryOrderView.as_view(), name='history_order'),  # история заказов пользователя
+    path('index/', IndexView.as_view(), name='index'),  # страница с популярными товарами
+    path('one_order/', OneOrderView.as_view(), name='one_order'),  # страница  заказа
+    path('order/', OrderView.as_view(), name='order'),  # страница оформления заказа
+    path('payment/', PaymentView.as_view(), name='payment'),  # страница оплаты(ввода карты) заказа
+    path('payment_some_one/', PaymentSomeOneView.as_view(), name='payment_some_one'),
+    # страница оплаты(генерация счета) заказа
 
-    path('products/', AllProductView.as_view(), name='products'),  # каталог товаров с фильтром и сортировкой
-    path('products/<int:pk>/', OneProductBuy.as_view(), name='product_buy'),  # страница товара
-    path('basket/', BasketView.as_view(), name='basket'),  # корзина пользователя
-    path('buy/', BuyBasket.as_view(), name='buy'),  # оформление и оплата заказа
+    path('products/<int:pk>/', ProductView.as_view(), name='product'),  # страница товара
+    path('profile_edit/', ProfileEditView.as_view(), name='profile_edit'),  # редактирование профиля пользователя
+    path('profile_avatar/', ProfileEditAvatarView.as_view(), name='profile_avatar'),
+    # редактирование профиля(с загруженным аватаром) пользователя
+
+    path('progress_payment/', ProgressPaymentView.as_view(), name='progress_payment'),
+    # страница ожидания (загрузки) оплаты
+
+    path('sale/', SaleView.as_view(), name='sale'),  # страница с товарами со скидкой
 ]
